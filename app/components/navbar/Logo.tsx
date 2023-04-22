2bbb3b6e028f0a9607c46b9319ddb6cb5a8ef422
NicoC0 <nicoconfaa@gmail.com>
2023-04-22 19:51:01 -0300

    finished the navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+const Logo = () => {
+  const router = useRouter()
+  return (
+    <Image src="/images/logo.png" className="hidden md:block cursor-pointer" alt="Logo" width={100} height={100} />
+  )
+}
+
+export default Logo
